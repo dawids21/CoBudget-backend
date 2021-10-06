5926078ea0fc1ca49632cfe9c8d9996f80764d7c
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-10-06 19:38:42 +0200

    add config for web security

---
+++ backend/src/main/java/xyz/stasiak/cobudget/security/DevOAuth2WebSecurityConfigurerAdapter.java
@@ -0,0 +1,19 @@
+package xyz.stasiak.cobudget.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Profile("dev")
+class DevOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .authorizeRequests().anyRequest().permitAll();
+    }
+
+}
---
+++ backend/src/main/java/xyz/stasiak/cobudget/security/OktaOAuth2WebSecurityConfigurerAdapter.java
@@ -0,0 +1,23 @@
+package xyz.stasiak.cobudget.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@Profile("!dev")
+class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .cors().disable() //TODO add cors
+                .oauth2ResourceServer().jwt();
+    }
+
+}
