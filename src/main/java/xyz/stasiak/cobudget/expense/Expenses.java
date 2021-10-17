2da169aeb4d18ee2a75410ec75f7b3587da4e7f9
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-10-17 20:07:50 +0200

    add query for retrieving expenses based on dates

--- src/main/java/xyz/stasiak/cobudget/expense/Expenses.java
+++ src/main/java/xyz/stasiak/cobudget/expense/Expenses.java
@@ -1,8 +1,15 @@
 package xyz.stasiak.cobudget.expense;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.time.LocalDate;
+import java.util.Set;
+
 @RepositoryRestResource
 interface Expenses extends Repository<Expense, Long> {
+
+    @Query("select * from expense where date >= #start and date <= #end")
+    Set<Expense> findByDateBetween(LocalDate start, LocalDate end);
 }
