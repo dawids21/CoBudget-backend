ed195b07085645cd6c7b7747ab14103163125df4
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-10-27 14:11:24 +0200

    change type of category in entry to aggregate ref

--- src/main/java/xyz/stasiak/cobudget/entry/Entry.java
+++ src/main/java/xyz/stasiak/cobudget/entry/Entry.java
@@ -1,9 +1,10 @@
 package xyz.stasiak.cobudget.entry;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
 
 import java.time.LocalDate;
 
-record Entry(@Id Long id, int amount, LocalDate date, String category, String subcategory) {
+record Entry(@Id Long id, int amount, LocalDate date, AggregateReference<Category, Long> category, String subcategory) {
 
 }
