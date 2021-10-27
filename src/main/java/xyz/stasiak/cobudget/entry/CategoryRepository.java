220d40806a789d5fa96f0c3c7743fb2b20d0fd12
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-10-27 20:53:45 +0200

    remove spring data rest for Category

--- src/main/java/xyz/stasiak/cobudget/entry/CategoryRepository.java
+++ src/main/java/xyz/stasiak/cobudget/entry/CategoryRepository.java
@@ -2,9 +2,8 @@ package xyz.stasiak.cobudget.entry;
 
 import io.vavr.control.Option;
 import org.springframework.data.repository.Repository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@org.springframework.stereotype.Repository
 interface CategoryRepository extends Repository<Category, Long> {
 
     Category save(Category category);
