b589c074649e990184784619d50daa341871c23b
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-07 12:16:28 +0100

    remove dev security config

--- src/main/java/xyz/stasiak/cobudget/config/security/OktaOAuth2WebSecurityConfigurerAdapter.java
+++ src/main/java/xyz/stasiak/cobudget/config/security/OktaOAuth2WebSecurityConfigurerAdapter.java
@@ -1,14 +1,12 @@
 package xyz.stasiak.cobudget.config.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-@Profile("!dev")
 class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
     @Override
