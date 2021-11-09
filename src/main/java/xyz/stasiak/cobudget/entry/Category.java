ed5ef9d308be3b66c731c82109bccf1bd223674b
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-09 21:57:19 +0100

    add user_id to Category record

--- src/main/java/xyz/stasiak/cobudget/entry/Category.java
+++ src/main/java/xyz/stasiak/cobudget/entry/Category.java
@@ -2,5 +2,5 @@ package xyz.stasiak.cobudget.entry;
 
 import org.springframework.data.annotation.Id;
 
-record Category(@Id Long id, String name) {
+record Category(@Id Long id, String user_id, String name) {
 }
