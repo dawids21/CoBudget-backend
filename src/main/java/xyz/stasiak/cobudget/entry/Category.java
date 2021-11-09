075ef9d9ce0a980ff49358125858b78fef41e02c
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-09 22:20:04 +0100

    add @JsonIgnore on userId fields

--- src/main/java/xyz/stasiak/cobudget/entry/Category.java
+++ src/main/java/xyz/stasiak/cobudget/entry/Category.java
@@ -1,8 +1,9 @@
 package xyz.stasiak.cobudget.entry;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.data.annotation.Id;
 
-record Category(@Id Long id, String user_id, String name) {
+record Category(@Id Long id, @JsonIgnore String user_id, String name) {
 
     static Category of(CategoryWriteModel dto, String userId) {
         return new Category(null, userId, dto.name());
