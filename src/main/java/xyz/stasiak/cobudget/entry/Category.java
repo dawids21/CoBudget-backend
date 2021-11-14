004deacf8f4fdba0d3f1111e04d0de2875ac2f6c
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-14 12:36:40 +0100

    add parentId to category record

--- src/main/java/xyz/stasiak/cobudget/entry/Category.java
+++ src/main/java/xyz/stasiak/cobudget/entry/Category.java
@@ -3,10 +3,10 @@ package xyz.stasiak.cobudget.entry;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.data.annotation.Id;
 
-record Category(@Id Long id, @JsonIgnore String user_id, String name) {
+record Category(@Id Long id, @JsonIgnore String userId, Long parentId, String name) {
 
     static Category of(CategoryWriteModel dto, String userId) {
-        return new Category(null, userId, dto.name());
+        return new Category(null, userId, dto.parentId(), dto.name());
     }
 
 }
