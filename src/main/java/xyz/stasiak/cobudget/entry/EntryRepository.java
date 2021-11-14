07ec1d4c6f4c34041385aab62f6be698c01cc0da
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-14 13:05:14 +0100

    add second join in EntryRepository

--- src/main/java/xyz/stasiak/cobudget/entry/EntryRepository.java
+++ src/main/java/xyz/stasiak/cobudget/entry/EntryRepository.java
@@ -12,9 +12,10 @@ interface EntryRepository extends Repository<Entry, Long> {
     Entry save(Entry expense);
 
     @Query("""
-            select entry.id, amount, date, category.name as category, subcategory
+            select entry.id, amount, date, category.name as category, subcategory.name as subcategory
             from entry
-            left join category on category.id = entry.category_id
+            left join category subcategory on subcategory.id = entry.category_id
+            left join category on category.id = subcategory.parent_id
             where entry.user_id like :userId and date >= :start and date <= :end
             """)
     Set<EntryReadModel> findByDateBetween(LocalDate start, LocalDate end, String userId);
