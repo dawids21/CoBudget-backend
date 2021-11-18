86a6b762748e9fcdd1a2d4a20c84ecf1566c8bb5
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-18 18:54:00 +0100

    add category name to get all categories

--- src/main/java/xyz/stasiak/cobudget/category/CategorySubcategoryProjection.java
+++ src/main/java/xyz/stasiak/cobudget/category/CategorySubcategoryProjection.java
@@ -7,7 +7,7 @@ import xyz.stasiak.cobudget.category.dto.CategorySubcategoryReadModel;
 record CategorySubcategoryProjection(long categoryId, String category, long subcategoryId, String subcategory) {
 
     static CategorySubcategoryReadModel toCategorySubcategoryReadModel(List<CategorySubcategoryProjection> projections) {
-        return new CategorySubcategoryReadModel(projections.get().categoryId, projections.map(CategorySubcategoryProjection::toCategoryReadModel));
+        return new CategorySubcategoryReadModel(projections.get().categoryId, projections.get().category, projections.map(CategorySubcategoryProjection::toCategoryReadModel));
     }
 
     private CategoryReadModel toCategoryReadModel() {
