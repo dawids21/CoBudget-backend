27f8a2c9d1743e8698fc3f182769562942965947
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-18 20:42:27 +0100

    create categories with subcategories programmatically

--- src/main/java/xyz/stasiak/cobudget/category/CategoryRepository.java
+++ src/main/java/xyz/stasiak/cobudget/category/CategoryRepository.java
@@ -1,7 +1,6 @@
 package xyz.stasiak.cobudget.category;
 
 import io.vavr.collection.List;
-import io.vavr.collection.Set;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.Repository;
 import xyz.stasiak.cobudget.category.dto.CategoryReadModel;
@@ -12,15 +11,18 @@ interface CategoryRepository extends Repository<Category, Long> {
     Category save(Category category);
 
     @Query("select id, null, name from category where parent_id is null and user_id = :userId")
-    Set<CategoryReadModel> findCategories(String userId);
+    List<CategoryReadModel> findCategories(String userId);
+
+    @Query("select id, parent_id, name from category where parent_id is not null and user_id = :userId")
+    List<CategoryReadModel> findSubcategories(String userId);
 
     @Query("select id, parent_id, name from category where parent_id = :parentId and user_id = :userId")
-    Set<CategoryReadModel> findSubcategories(String userId, Long parentId);
+    List<CategoryReadModel> findSubcategories(String userId, Long parentId);
 
     @Query("""
             select category.id as category_id, category.name as category, subcategory.id as subcategory_id, subcategory.name as subcategory
             from category
-            inner join category subcategory on subcategory.parent_id = category.id
+            left join category subcategory on subcategory.parent_id = category.id
             where category.user_id = :userId
             """)
     List<CategorySubcategoryProjection> findAllCategories(String userId);
--- src/main/java/xyz/stasiak/cobudget/category/CategorySubcategoryProjection.java
+++ src/main/java/xyz/stasiak/cobudget/category/CategorySubcategoryProjection.java
@@ -4,10 +4,16 @@ import io.vavr.collection.List;
 import xyz.stasiak.cobudget.category.dto.CategoryReadModel;
 import xyz.stasiak.cobudget.category.dto.CategorySubcategoryReadModel;
 
-record CategorySubcategoryProjection(long categoryId, String category, long subcategoryId, String subcategory) {
+record CategorySubcategoryProjection(long categoryId, String category, Long subcategoryId, String subcategory) {
 
     static CategorySubcategoryReadModel toCategorySubcategoryReadModel(List<CategorySubcategoryProjection> projections) {
-        return new CategorySubcategoryReadModel(projections.get().categoryId, projections.get().category, projections.map(CategorySubcategoryProjection::toCategoryReadModel));
+        return new CategorySubcategoryReadModel(
+                projections.get().categoryId,
+                projections.get().category,
+                projections
+                        .filter(projection -> projection.subcategoryId != null)
+                        .map(CategorySubcategoryProjection::toCategoryReadModel)
+        );
     }
 
     private CategoryReadModel toCategoryReadModel() {
