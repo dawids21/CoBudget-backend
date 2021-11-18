88665e972d1fd2c08341b840d6693b3a24310a1c
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-18 18:02:22 +0100

    change query to get all categories

--- src/main/java/xyz/stasiak/cobudget/entry/CategoryRepository.java
+++ src/main/java/xyz/stasiak/cobudget/entry/CategoryRepository.java
@@ -10,18 +10,18 @@ interface CategoryRepository extends Repository<Category, Long> {
 
     Category save(Category category);
 
-    @Query("select id, name from category where parent_id is null and user_id = :userId")
+    @Query("select id, null, name from category where parent_id is null and user_id = :userId")
     Set<CategoryReadModel> findCategories(String userId);
 
-    @Query("select id, name from category where parent_id = :parentId and user_id = :userId")
+    @Query("select id, parent_id, name from category where parent_id = :parentId and user_id = :userId")
     Set<CategoryReadModel> findSubcategories(String userId, Long parentId);
 
     @Query("""
-            select category.id, category.name, subcategory.id, subcategory.name
+            select category.id as category_id, category.name as category, subcategory.id as subcategory_id, subcategory.name as subcategory
             from category
-            left join category subcategory on subcategory.parent_id = category.id
+            inner join category subcategory on subcategory.parent_id = category.id
             where category.user_id = :userId
             """)
-    List<SubcategoryReadModel> findAllCategories(String userId);
+    List<CategorySubcategoryReadModel> findAllCategories(String userId);
 
 }
