55d428583277e9a370c9ebd4d171f8367aa030b1
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-01-19 19:43:15 +0100

    handle category id not found exception in controller

--- src/main/java/xyz/stasiak/cobudget/category/CategoryApplicationService.java
+++ src/main/java/xyz/stasiak/cobudget/category/CategoryApplicationService.java
@@ -1,9 +1,11 @@
 package xyz.stasiak.cobudget.category;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import xyz.stasiak.cobudget.category.exception.CategoryIdNotFound;
 
 @RequiredArgsConstructor
+@Slf4j
 class CategoryApplicationService {
 
     private final CategoryRepository repository;
@@ -15,6 +17,7 @@ class CategoryApplicationService {
     void disable(long id) {
         var category = repository.findById(id).getOrElseThrow(() -> new CategoryIdNotFound(id));
         category.setDisabled(true);
+        log.debug("Disabled category with id {}", id);
         repository.save(category);
     }
 }
--- src/main/java/xyz/stasiak/cobudget/category/CategoryController.java
+++ src/main/java/xyz/stasiak/cobudget/category/CategoryController.java
@@ -1,10 +1,13 @@
 package xyz.stasiak.cobudget.category;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
+import xyz.stasiak.cobudget.category.exception.CategoryIdNotFound;
+import xyz.stasiak.cobudget.common.ErrorMessage;
 import xyz.stasiak.cobudget.common.UserId;
 import xyz.stasiak.cobudget.common.UserIdNotFound;
 
@@ -12,6 +15,7 @@ import xyz.stasiak.cobudget.common.UserIdNotFound;
 @RequestMapping("/api/category")
 @RestController
 @CrossOrigin(origins = "${security.cors.origins}")
+@Slf4j
 class CategoryController {
 
     private final CategoryApplicationService categoryApplicationService;
@@ -25,7 +29,14 @@ class CategoryController {
 
     @DeleteMapping("/{id}")
     ResponseEntity<?> delete(@PathVariable long id) {
+        log.debug("Delete category with id {}", id);
         categoryApplicationService.disable(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(CategoryIdNotFound.class)
+    ResponseEntity<ErrorMessage> handleCategoryNotFound(CategoryIdNotFound exception) {
+        log.error(exception.getMessage(), exception);
+        return ResponseEntity.badRequest().body(ErrorMessage.of(exception));
     }
 }
