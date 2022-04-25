59ee55e6fdef824289806b3034bcdf02a41e0806
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-04-25 18:29:28 +0200

    create multi module project

---
+++ cobudget-category/src/test/groovy/xyz/stasiak/cobudget/category/query/DataIntegrationSpec.groovy
@@ -0,0 +1,19 @@
+package xyz.stasiak.cobudget.category.query
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.test.context.ActiveProfiles
+import spock.lang.Specification
+
+
+@DataJdbcTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+abstract class DataIntegrationSpec extends Specification {
+
+    @Autowired
+    protected JdbcTemplate jdbcTemplate
+
+}
