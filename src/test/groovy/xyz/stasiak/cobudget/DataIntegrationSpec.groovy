edcf887a12020568407ec2d07a7fa896fec9c0fd
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-05-07 10:30:26 +0200

    add Testcontainers to run database in integration tests

--- src/test/groovy/xyz/stasiak/cobudget/DataIntegrationSpec.groovy
+++ src/test/groovy/xyz/stasiak/cobudget/DataIntegrationSpec.groovy
@@ -5,12 +5,14 @@ import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.ActiveProfiles
+import org.testcontainers.spock.Testcontainers
 import spock.lang.Specification
 
 
 @DataJdbcTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
+@Testcontainers
 abstract class DataIntegrationSpec extends Specification {
 
     @Autowired
