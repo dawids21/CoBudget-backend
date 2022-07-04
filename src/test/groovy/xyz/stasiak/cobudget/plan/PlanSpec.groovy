2e1014c23bd7c3bc7bf486d1d8311fa905532cc2
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-07-04 17:08:13 +0200

    add test for getting year and month from plan

--- src/test/groovy/xyz/stasiak/cobudget/plan/PlanSpec.groovy
+++ src/test/groovy/xyz/stasiak/cobudget/plan/PlanSpec.groovy
@@ -9,8 +9,7 @@ class PlanSpec extends Specification {
 
     def "should add plan for category"() {
         given:
-        def plan = new Plan("user", LocalDate.of(2022, Month.JULY, 1))
-        plan.setId(1L)
+        def plan = aPlan()
 
         when:
         plan.planCategory(2L, 300)
@@ -18,5 +17,22 @@ class PlanSpec extends Specification {
         then:
         def plannedCategory = plan.getPlanForCategory(2L)
         plannedCategory.getAmount() == 300
+    }
+
+    def "plan should have year and month specified"() {
+        given:
+        def plan = aPlan()
+
+        expect:
+        with(plan.getYearAndMonth()) {
+            getYear() == 2022
+            getMonth() == Month.JULY
+        }
+    }
+
+    def aPlan() {
+        def plan = new Plan("user", LocalDate.of(2022, Month.JULY, 1))
+        plan.setId(1L)
+        return plan
     }
 }
