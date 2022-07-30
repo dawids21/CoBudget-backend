78f3e8fc506e8d5af458bd587b10e75cfa2a830a
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-07-30 17:36:49 +0200

    Revert "enable spring security debug"
    This reverts commit deb5381d7d976ffea8e062d534c7fa977df92f8e.

--- src/main/java/xyz/stasiak/cobudget/BackendApplication.java
+++ src/main/java/xyz/stasiak/cobudget/BackendApplication.java
@@ -2,10 +2,8 @@ package xyz.stasiak.cobudget;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity(debug = true)
 public class BackendApplication {
 
     public static void main(String[] args) {
