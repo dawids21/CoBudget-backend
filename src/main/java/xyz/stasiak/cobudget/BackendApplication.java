deb5381d7d976ffea8e062d534c7fa977df92f8e
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-07-30 17:33:46 +0200

    enable spring security debug

--- src/main/java/xyz/stasiak/cobudget/BackendApplication.java
+++ src/main/java/xyz/stasiak/cobudget/BackendApplication.java
@@ -2,8 +2,10 @@ package xyz.stasiak.cobudget;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class BackendApplication {
 
     public static void main(String[] args) {
