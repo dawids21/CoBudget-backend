96b40194a0244309d735690ea6a8131b04f580b0
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-08-05 17:24:17 +0200

    chore: Migrate to Spring Boot 2.7

--- src/main/java/xyz/stasiak/cobudget/config/security/WebSecurityConfig.java
+++ src/main/java/xyz/stasiak/cobudget/config/security/WebSecurityConfig.java
@@ -1,17 +1,17 @@
 package xyz.stasiak.cobudget.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+class WebSecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/actuator/health")
@@ -22,6 +22,6 @@ class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapte
                 .and()
                 .csrf().disable()
                 .oauth2ResourceServer().jwt();
+        return http.build();
     }
-
 }
