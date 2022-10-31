61f310eaebb7d60210730691e3e79bf98ca44f5d
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-10-31 16:34:24 +0100

    Bump versions

--- check-updates.sh
+++ check-updates.sh
@@ -2,5 +2,4 @@
 ./mvnw versions:display-parent-updates \
   versions:display-dependency-updates \
   versions:display-plugin-updates \
-  versions:display-property-updates \
   -DprocessDependencyManagement=false
