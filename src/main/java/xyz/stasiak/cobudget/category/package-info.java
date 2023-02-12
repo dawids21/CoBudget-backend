d8c166ca45c02ea8fbab963f412f68a51246aec1
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-02-12 12:21:04 +0100

    feat: Add Spring Modulith features

---
+++ src/main/java/xyz/stasiak/cobudget/category/package-info.java
@@ -0,0 +1,4 @@
+@ApplicationModule(allowedDependencies = "common")
+package xyz.stasiak.cobudget.category;
+
+import org.springframework.modulith.ApplicationModule;
---
+++ src/main/java/xyz/stasiak/cobudget/common/package-info.java
@@ -0,0 +1,4 @@
+@ApplicationModule
+package xyz.stasiak.cobudget.common;
+
+import org.springframework.modulith.ApplicationModule;
