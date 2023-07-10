ebdcdc3c24736dd95f22d08491b36b11fb7ea905
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-07-10 15:28:10 +0200

    Create GitHub action

--- build-image.sh
+++ build-image.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=cobudget/backend
+./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=registry.vm.stasiak.xyz/cobudget
